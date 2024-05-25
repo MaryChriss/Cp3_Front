6112832be0116c296194e652d554de46b8820f1a
Mariana <marychris.crf@gmail.com>
2024-05-25 13:56:50 -0300

    Salvando a tarefa no localStorage

--- js/script.js
+++ js/script.js
@@ -1,20 +1,33 @@
-document.addEventListener('DOMContentLoaded',
-
-    function () {
+document.addEventListener('DOMContentLoaded', function () {
     const todoForm = document.getElementById('todo-form');
     const todoInput = document.getElementById('new-task');
     const todoList = document.getElementById('todo-list');
+
+    function loadTasks() {
+        const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        savedTasks.forEach(task => {
+            createTaskElement(task);
+        });
+    }
+
+    function createTaskElement(taskText) {
+        const newTaskElement = document.createElement('li');
+        newTaskElement.textContent = taskText;
+        todoList.appendChild(newTaskElement);
+    }
+
+    loadTasks();
 
     todoForm.addEventListener('submit', function (event) {
         event.preventDefault(); 
 
         const newTaskText = todoInput.value.trim();
+        if (newTaskText !== '') {
+            createTaskElement(newTaskText);
 
-        if (newTaskText !== '') { 
-            const newTaskElement = document.createElement('li');
-            newTaskElement.textContent = newTaskText;
-
-            todoList.appendChild(newTaskElement);
+            const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+            savedTasks.push(newTaskText);
+            localStorage.setItem('tasks', JSON.stringify(savedTasks));
 
             todoInput.value = '';
         }
